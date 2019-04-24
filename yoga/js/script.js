c8c9c616ca0935b9739485365bbbcbc3fb302be3
Nata <n.prisyazhnaya@gmail.com>
2019-04-24 11:53:47 +0200

    slider hW15

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -134,43 +134,104 @@ window.addEventListener('DOMContentLoaded', function() {
        failure: 'Что-то пошло не так...'
      };
 
-     let form = document.querySelector('.main-form'),
-         input = form.getElementsByTagName('input'),
-         statusMessage = document.createElement('div');
+    
+    let form = document.querySelector('.main-form'),
+    formDown = document.querySelector('#form'),
+    input = form.getElementsByTagName ('input'),
+    statusMessage = document.createElement('div');
+     
+ 
+    statusMessage.classList.add('status');
+ 
+function sendForm(elem){
+ 
+    elem.addEventListener('submit', function (event) {
+        event.preventDefault();
+        elem.appendChild(statusMessage);
+    
+        let formData = new FormData(elem);
+    
+    function postData() {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+            request.onreadystatechange = function () {
+                if(request.readyState < 4) {
+                    resolve();
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                }
+            };
+            request.send(formData); // Вот тут другое название переменной
+        });
+    }
+        function clearInput() {
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = ' ';
+        }
+    }
+    postData (formData)
+        .then (() => statusMessage.innerHTML = message.loading)
+        .then (() => statusMessage.innerHTML = message.success)
+        .catch (() => statusMessage.innerHTML = message.failure)
+        .then (clearInput);
+    });
+}
+sendForm(form);
+sendForm(formDown);
 
-         statusMessage.classList.add('status');
+// Slider
 
-         form.addEventListener('submit', function(event){
-           event.preventDefault();
-           form.appendChild(statusMessage);
+let slideIndex = 1,
+    slides = document.querySelectorAll('.slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
 
-           let request = new XMLHttpRequest();
-           request.open('POST', 'server.php');
-           // request.setRequestHeader('Content-Type', 'aplication/x-wwww-form-urlencoded');
-           request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    showSlides(slideIndex);
+    function showSlides(n) {
 
-           let formData = new FormData(form);
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach((item) => item.style.display = 'none');
 
-           let obj = {};
-           formData.forEach(function(value, key){
-             obj[key] = value;
-           });
-           let json = JSON.stringify(obj);
-           request.send(json);
+        dots.forEach((item) => item.classList.remove('dot-active'));
 
-           request.addEventListener('readystatechange', function(){
-             if (request.readyState < 4){
-               statusMessage.innerHTML = message.loading;
-             } else if (request.readyState === 4 && request.status === 200){
-              statusMessage.innerHTML = message.success;
-             } else {
-              statusMessage.innerHTML = message.failure;
-             }
-           });
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
 
-           for (let i = 0; i < input.length; i++){
-             input[i].value = '';
-           }
+    }
 
-         });
+    function plusSlide(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    // strelki
+    prev.addEventListener('click', function() {
+        plusSlide(-1);
+    });
+    next.addEventListener('click', function() {
+        plusSlide(1);
+    });
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length +1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]){
+                currentSlide(i);
+            }
+        }
+    });
+    
 });
