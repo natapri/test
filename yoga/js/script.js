cfb5dc84ec52a35454f4d1015a1c4f4fd869b403
Nata <n.prisyazhnaya@gmail.com>
2019-04-24 13:57:50 +0200

    homeW16

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -233,5 +233,50 @@ let slideIndex = 1,
             }
         }
     });
-    
+
+    // Calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+         
+        
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        // total = (daysSum + personsSum)*4000;
+
+        if(restDays.value == '' || personsSum == 0 || daysSum == 0 ) {
+            totalValue.innerHTML = 0;
+        } else {
+            total = (daysSum + personsSum)*4000;
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        // total = (daysSum + personsSum)*4000;
+
+        if(persons.value == '' || daysSum == 0 || personsSum == 0) {
+            totalValue.innerHTML = 0;
+        } else {
+            total = (daysSum + personsSum)*4000;
+            totalValue.innerHTML = total;
+        }
+    });
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '' || daysSum == 0 || personsSum == 0){
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+
+        }
+    });
 });
